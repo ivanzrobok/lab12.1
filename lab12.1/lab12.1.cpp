79b6e4f7f2f3dcea7cbab11bd8e768b9866ed46a
ivanzrobok <ivan.zrobok.itis.2020@lpnu.ua>
2020-12-28 13:04:07 +0200

    first

---
+++ lab12.1/lab12.1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Elem
+{
+	Elem* link1,
+		* link2;
+	int info;
+};
+Elem* p;
+
+
+int main()
+{
+	p = new Elem;
+	p->info = 1;
+	p->link1 = new Elem;
+	p->link1->info = 2;
+	p->link1->link1 = NULL;	
+	p->link1->link2 = new Elem;
+	p->link1->link2->info = 3;
+	p->link1->link2->link2 = NULL;
+	p->link1->link2->link1 = new Elem;
+	p->link1->link2->link1->info = 4;
+	p->link1->link2->link1->link1 = p->link1;
+	p->link1->link2->link1->link2 = p;
+	
+	delete p->link1->link2->link1;
+	delete p->link1->link2;
+	delete p->link1;
+	delete p;
+
+	return 0;
+}
